50b1cf880e28285a284b6ea9af72cde683618321
maj0r <54349566+maj0roff@users.noreply.github.com>
2021-09-01 11:22:54 +0500

    Update in calculator
    Added Lenght by diametr
    Added userinput / gui
    Added user panel

--- main.cpp
+++ main.cpp
@@ -4,23 +4,90 @@
 
 using namespace std;
 
-void calc()
+void calc_by_radius()
 {
 	number = (nump * 2) * radius;
 }
 
+void calc_by_diametr()
+{
+	number2 = nump * diametr;
+}
+
+
 int main()
 {
-	
+	//Setup console
 	SetConsoleTitleA("Circle Lenght Calculator by maj0r");
 
-	cout << "Hello, write radius : ";
-	cin >> radius;
-	calc();
-	cout << "Lenght : " << number << "\n";
+	cout << "Circle lenght calculator by maj0r\n";
+	cout << "Select :\n";
+	cout << "[1] Lenght By Radius\n";
+	cout << "[2] Lenght by Diametr\n";
+	cout << "[3] Info panel\n";
+	cout << "\n";
+	cout << "User input :";
+	cin >> userinput; // Getting userinput
 
-	system("pause");
+	if (userinput == 1)
+	{
+		cout << "\n";
+		cout << "\n";
+		cout << "Write the circle radius\n";
+		cout << "User input : ";
 
-	return 0;
+		cin >> radius; // Getting userinput
+
+		calc_by_radius(); // Calling main calculate function
+		cout << "Circle lenght is " << number << "\n"; // Sending output with answer
+
+		cout << "\n";
+		system("pause");
+
+	}
+	else if (userinput == 2)
+	{
+		cout << "\n";
+		cout << "\n";
+		cout << "Write the circle diametr\n";
+		cout << "User input : ";
+
+		cin >> diametr; // Getting userinput
+
+		calc_by_diametr(); // Calling main calculate function 
+		cout << "Circle radius is " << number2 << "\n"; // Sending output with answer
+
+		cout << "\n";
+		system("pause");
+
+	}
+	else if (userinput == 3)
+	{
+		cout << "\n";
+		cout << "\n";
+		cout << "Hello this is circle calculator by maj0r\n";
+		cout << "Here you can calculate circle lenght and radius\n";
+		cout << "And you can write float ints like this : 3.1415\n";
+		cout << "\n";
+		system("pause");
+
+	}
+	/*else if (userinput == 4)
+	{
+		cout << "\n";
+		cout << "Good bye!\n";
+		cout << "See ya later!\n";
+		cout << "\n";
+		system("pause");
+		system("exit");
+	}*/
+	else
+	{
+		cout << "\n";
+		cout << "Invalid input!\n";
+		cout << "\n";
+		system("pause");
+
+	}
 
 }
